c38372c760052f3271011ff517b886d65f4b1aa3
dernorgan <dernorgan@gmail.com>
2025-11-08 13:18:53 +0200

    services

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ const port = 3000;
 const cors = require('./middleware/cors');
 app.use(cors()); // ← викликаємо функцію!
 
-const fileRoutes = require('./routes/files');
-const uploadRoutes = require('./routes/upload');
+const fileRoutes = require('./routes/getMusics');
+const updateFirebaseRoutes = require('./routes/updateFireBase');
+const uploadRoutes = require('./routes/uploadMusic');
+const databaseRoutes = require('./routes/database');
 
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('🎶 Ласкаво просимо на мій аудіо-сервер!');
 });
 
 app.use('/', fileRoutes);
+app.use('/', updateFirebaseRoutes);
 app.use('/', uploadRoutes);
+app.use('/db', databaseRoutes);
 
 app.listen(port, () => {
     console.log(`✅ Server running: http://localhost:${port}`);
