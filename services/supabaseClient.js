de664e33da4b2b53a03f141ac7789bf8e5323311
dernorgan <dernorgan@gmail.com>
2025-11-08 13:25:14 +0200

    services

--- services/supabaseClient.js
+++ services/supabaseClient.js
@@ -7,3 +7,11 @@ const supabase = createClient(
 );
 
 module.exports = supabase;
+
+//
+// const SUPABASE_URL = process.env.SUPABASE_URL;        // URL твого проєкту Supabase
+// const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY; // Анонімний ключ API (або service key)
+//
+// const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+//
+// module.exports = supabaseClient;
